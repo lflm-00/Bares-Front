43166119066e01d68ac9e85bec01ef81f3f13cea
Emmanuel-G <emmanuel.grajales@compra123.com>
2021-11-19 15:58:33 -0500

    juan yta este es el fondo de 404

--- src/components/User/NotFound.js
+++ src/components/User/NotFound.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import image from "../../img/noFound.gif";
+import scanor from "../../img/juanScanor.gif";
+import Navbar from "../Home/Navbar";
+
 
 export default function NotFound() {
-    return (
-        <div>
-            <h1>juano not found</h1>
-        </div>
-    )
+  return (
+    <>
+      <Navbar />
+
+      <div>
+          <img  src={image}/>
+      </div>
+      <div>
+          <img src={scanor} />
+      </div>
+    </>
+  );
 }
